a9a5a9689408ef53c760704d46fc3a3b96c6e445
mirceaciu <mirceaciu@gmail.com>
2016-04-29 16:49:54 +0300

    quake anchir

--- res/leaflet-custom.js
+++ res/leaflet-custom.js
@@ -1,5 +1,1 @@
-// Create the map
-var map = L.map('map').setView([44.42562793818246, 26.069784164428707], 12);
 
-// Set up the OSM layer
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom: 18}).addTo(map);
--- res/quake.js
+++ res/quake.js
@@ -1,10 +1,16 @@
+// Create the map
+var map = L.map('map').setView([45.68315803253308, 26.444091796874996], 10);
+
+// Set up the OSM layer
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom: 18}).addTo(map);
+
 var cssIcon = L.divIcon({
 		  // Specify a class name we can refer to in CSS.
 		  className: 'css-icon',
 		  html: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="50px" height="50px" viewBox="-25 -25 50 50" enable-background="new 0 0 50 50" xml:space="preserve"><circle id="fill" fill="#ED1C24" cx="0" cy="0" r="4"/></svg>'
 		  // Set marker width and height
-		  ,iconSize: [50,50]
-		  ,iconAnchor: [50,50]
+		  ,iconSize: [10,10]
+		  ,iconAnchor: [10,10]
 		});
 
 
@@ -19,9 +25,4 @@ var player = document.getElementById('fill').animate([
 		iterations: Infinity, //or a number
 		direction: 'normal', //'normal', 'reverse', etc.
 		fill: 'auto' //'backwards', 'both', 'none', 'auto'
-	});
-
-
-map.on('zoomend', function() {
-      console.log(map.getZoom());
 });
